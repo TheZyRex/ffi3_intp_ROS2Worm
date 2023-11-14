823568569a87886d40140396b2e79da825f37905
Mario <marioross3503@gmail.com>
2023-11-14 16:38:54 +0100

    python init try

---
+++ src/ros2_worm_multiplayer/scripts/py_node.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import rclpy
+from ros2_worm_multiplayer.module_to_import import MyNode
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
