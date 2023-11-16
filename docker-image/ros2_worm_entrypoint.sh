d2391d65189453cad60f43c7df79e9243e26e766
Joshua Peller <jopel@jopel.de>
2023-11-16 12:49:03 +0100

    feat: docker-compose not working in networkmode right now

--- docker-image/ros2_worm_entrypoint.sh
+++ docker-image/ros2_worm_entrypoint.sh
@@ -9,8 +9,13 @@ if [ -d "$ROS2_INSTALL_DIR" ]; then
     source "$ROS2_INSTALL_DIR"setup.bash
     echo "Sourced ROS 2 setup.bash from $ROS2_INSTALL_DIR"
 
-		cd /home/ffi3/ffi3_intp/ros2 || exit
+		cd /home/ffi3/ffi3_intp_ROS2Worm || exit
 		colcon build
+
+		if [ -d "/home/ffi3/ffi3_intp_ROS2Worm/install/" ]; then
+			source /home/ffi3/ffi3_intp_ROS2Worm/install/setup.bash
+			ros2 run ros2_worm_multiplayer worm_grid_node
+		fi
 else
     echo "Error: ROS 2 installation directory not found: $ROS2_INSTALL_DIR"
 fi
